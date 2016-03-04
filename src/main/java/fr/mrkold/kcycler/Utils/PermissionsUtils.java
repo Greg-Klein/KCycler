4ffd42f0a5c7f30901e5751c3641173d2e01efe9
Greg-Klein <gregoryklein@free.fr>
2016-03-04 15:46:54 +0100

    Added PlotSquared compatibility

--- src/main/java/fr/mrkold/kcycler/Utils/PermissionsUtils.java
+++ src/main/java/fr/mrkold/kcycler/Utils/PermissionsUtils.java
@@ -3,6 +3,8 @@ package fr.mrkold.kcycler.Utils;
 import org.bukkit.Location;
 import org.bukkit.entity.Player;
 
+import com.intellectualcrafters.plot.api.PlotAPI;
+import com.intellectualcrafters.plot.object.Plot;
 import com.sk89q.worldguard.bukkit.WorldGuardPlugin;
 
 import fr.mrkold.kcycler.KCyclerPlugin;
@@ -13,11 +15,13 @@ public class PermissionsUtils implements PluginConstants {
 	KCyclerPlugin plugin;
 	PluginUtils pluginUtils;
 	WorldGuardPlugin worldguard;
+	PlotAPI plotsquared;
 
 	public PermissionsUtils(KCyclerPlugin plugin) {
 		this.plugin = plugin;
 		pluginUtils = plugin.getPluginUtils();
 		worldguard = pluginUtils.getWorldGuard();
+		plotsquared = pluginUtils.getPlotSquared();
 	}
 
 	public boolean canBuildAt(Player player, Location location) {
@@ -34,6 +38,34 @@ public class PermissionsUtils implements PluginConstants {
 		if (worldguard != null) {
 			if (!worldguard.canBuild(player, location)) {
 				return false;
+			}
+		}
+
+		/**
+		 * Return false if player can't build in this PlotSquared plot
+		 */
+		if (plotsquared != null) {
+			/**
+			 * Is this location in a plotworld
+			 */
+			if (plotsquared.isPlotWorld(location.getWorld())) {
+				/**
+				 * If location is on a plot
+				 */
+				Plot plot = plotsquared.getPlot(location);
+				if (plot != null) {
+					/**
+					 * Return false if player isn't added to the plot
+					 */
+					if (!plot.isAdded(player.getUniqueId())) {
+						return false;
+					}
+				} else {
+					/**
+					 * Return false if player is in plotworld but not in plot
+					 */
+					return false;
+				}
 			}
 		}
 
--- src/main/java/fr/mrkold/kcycler/Utils/PluginUtils.java
+++ src/main/java/fr/mrkold/kcycler/Utils/PluginUtils.java
@@ -2,11 +2,13 @@ package fr.mrkold.kcycler.Utils;
 
 import org.bukkit.plugin.Plugin;
 
+import com.intellectualcrafters.plot.api.PlotAPI;
 import com.sk89q.worldguard.bukkit.WorldGuardPlugin;
 
 import fr.mrkold.kcycler.KCyclerPlugin;
+import fr.mrkold.kcycler.PluginConstants;
 
-public class PluginUtils {
+public class PluginUtils implements PluginConstants {
 
 	private KCyclerPlugin plugin;
 
@@ -18,11 +20,29 @@ public class PluginUtils {
 	 * Detect if Worldguard is enabled
 	 * 
 	 * @return Worldguard instance
-	 * @throws Exception
 	 */
 	public WorldGuardPlugin getWorldGuard() {
-		Plugin wg = plugin.getServer().getPluginManager().getPlugin("WorldGuard");
+		Plugin wg = plugin.getServer().getPluginManager().getPlugin(WORLDGUARD_PLUGIN_NAME);
+		if (wg != null) {
+			plugin.getLogger().info("- Worldguard detected");
+		}
 		return (WorldGuardPlugin) wg;
+	}
+
+	/**
+	 * Detect if PlotSquared is enabled
+	 * 
+	 * @return PlotSquared API instance
+	 */
+	@SuppressWarnings("deprecation")
+	public PlotAPI getPlotSquared() {
+		PlotAPI api = null;
+		Plugin ps = plugin.getServer().getPluginManager().getPlugin(PLOTSQUARED_PLUGIN_NAME);
+		if (ps != null) {
+			plugin.getLogger().info("- PlotSquared detected");
+			api = new PlotAPI();
+		}
+		return api;
 	}
 
 }
